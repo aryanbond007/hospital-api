0d0e2cd512a3f98ee058c084738673970c5a84e0
Aryan raj <araj8764@gmail.com>
2024-07-21 21:19:04 +0530

    some minor bugs

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,9 +5,14 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
+const dotenv = require('dotenv').config();
 
+//Set up default mongoose connection
+const mongoDB = process.env.DEVELOPMENT_DB;
+module.exports = mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("CONNECTION ESTABLISHED"));
 //name of the db is products-api
-mongoose.connect('mongodb+srv://araj8764:oWLDyam0j2VLB00k@cluster0.aatwmd2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+// mongoose.connect('mongodb+srv://araj8764:oWLDyam0j2VLB00k@cluster0.aatwmd2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+// mongoose.connect(process.env.db);
 
 const db=mongoose.connection;
 //for error
